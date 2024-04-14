a2fc800180cd2e2d49522e6e1aeb45dc70449e64
anyars-encarta <anyarsencarta@gmail.com>
2024-04-14 17:07:35 +0000

    Complete tooltip

---
+++ src/components/tooltip/Tooltip.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export const Tooltip = ({ text, children }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    return (
+        <div className='tooltip-container'
+            onMouseEnter={() => setIsVisible(true)}
+            onMouseLeave={() => setIsVisible(false)}
+        >
+            {children}
+            {
+                isVisible && <div className="tooltip">{text}</div>
+            }
+        </div>
+    )
+}
